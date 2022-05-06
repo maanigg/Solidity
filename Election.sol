b1b1fef3cc324edc4034162a9fec2e5d7d9abfe0
maanigg <52887003+maanigg@users.noreply.github.com>
2022-05-06 17:28:33 +0400

    Update Election.sol

--- Election.sol
+++ Election.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.2;
 
 contract Election {
     // Model a Candidate
+
     address public owner;
 
     constructor() public{
@@ -41,38 +42,24 @@ contract Election {
         addCandidate();
     }
 
-    function addCandidate () private {
+    function addCandidate () private {     
         candidates[candidatesCount] = newCandidate;
     }
 
     function vote (uint _candidateId) public {
         // require that they haven't voted before
         require(!voters[msg.sender],"Aldready Voted!");
-
-        bool candidateTrack = false;
-        uint currCandidate;
-        
-
-        // require a valid candidate
-        for(uint i = 1; i <= candidatesCount ; i++){
-            if(candidates[i].id == _candidateId){
-                candidateTrack = true;
-                currCandidate = i;
-            }
-        }
-        require(candidateTrack == true,"Candidate Does not exist");
-
+        // require candidate exists
+        require(_candidateId<candidatesCount && _candidateId > 0, "Candidate does not exist!");
         // record that voter has voted
         voters[msg.sender] = true;
-
         // update candidate vote Count
-        candidates[currCandidate].voteCount += 1;
-
+        candidates[_candidateId].voteCount += 1;
         // trigger voted event
         emit votedEvent(_candidateId);
     }
-    
-    //return winner ID and Name
+
+    //Return winner ID, Name and VoteCount
     function winner() Owner public view returns(uint, string memory, uint){
         uint winnerTrack = 1;
         for(uint j = 1; j<=candidatesCount; j++){
